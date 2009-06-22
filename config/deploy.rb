2127ac5cf31d67be23c019287611138e6dc4c65c
Fadi Eliya <fadi@li69-58.members.linode.com>
2009-06-22 18:09:38 -0400

    Updating Cap task for git

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,23 @@
-set :application, "twitchoice"
-set :repository,  "http://webfadi.com/svn/twitchoice"
+set :application, "twitchoice.com"
+set :repository,  "webfadi@webfadi.com:~/git/twitchoice.git"
+set :scm,         "git"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/var/www/webapps/#{application}"
-set :copy_exclude,  ['.svn']
+#set :deploy_via, "copy"
+#set :copy_cache, "#{ENV['HOME']}/deploy/#{application}"
+#set :copy_exclude,  ['.git']
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
 # set :scm, :subversion
 
-role :app, "qa.twitchoice.com"
-role :web, "qa.twitchoice.com"
-role :db,  "qa.twitchoice.com", :primary => true
+role :app, "twitchoice.com"
+role :web, "twitchoice.com"
+role :db,  "twitchoice.com", :primary => true
 
-set :user, "fadi"
-set :scm_username, "fadi"
-set :use_sudo, true 
+set :user, "deploy" #d3pl0y
+set :scm_username, "webfadi"
+set :use_sudo, false
