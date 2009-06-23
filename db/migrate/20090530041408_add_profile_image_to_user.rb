4e67d248b6c01f897203484702663688dd9daeda
Fadi Eliya <fadi@fadi-eliyas-macbook-pro.local>
2009-06-23 17:44:32 -0400

    Adding profile_image to user migration

--- db/migrate/20090530041408_add_profile_image_to_user.rb
+++ db/migrate/20090530041408_add_profile_image_to_user.rb
@@ -1,9 +1,18 @@
 class AddProfileImageToUser < ActiveRecord::Migration
   def self.up
     add_column :users, :image, :string
+    User.find(:all).each do |user| 
+      if user.twitter_id
+        user.image = Twitter.user(user.twitter_id).profile_image_url
+        user.save
+      end
+    end
   end
 
   def self.down
-    remove_column :users, :image, :string
+    remove_column :users, :image
   end
 end
+
+
+    
