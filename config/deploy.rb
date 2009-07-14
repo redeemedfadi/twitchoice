6436f1aebdc29134d368922ed4351b07036b8c33
Fadi Eliya <fadi@fadi-desktop.(none)>
2009-07-13 21:55:28 -0400

    Updating Capistrano task

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,9 +6,9 @@ set :scm,         "git"
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
 set :deploy_to, "/var/www/webapps/#{application}"
-#set :deploy_via, "copy"
+set :deploy_via, "copy"
 #set :copy_cache, "#{ENV['HOME']}/deploy/#{application}"
-#set :copy_exclude,  ['.git']
+set :copy_exclude,  ['.git']
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
@@ -21,3 +21,20 @@ role :db,  "twitchoice.com", :primary => true
 set :user, "deploy" #d3pl0y
 set :scm_username, "webfadi"
 set :use_sudo, false
+
+namespace :deploy do
+  %w(start restart).each do |name| 
+    task name, :roles => :app do 
+      passenger.restart
+    end
+  end
+end
+
+namespace :passenger do
+  desc "Restart Application"
+  task :restart do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+end
+
+after :deploy, "passenger:restart"
