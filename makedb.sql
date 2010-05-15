0f04062fa7f9b84b69fe972df2ccdcb0588e4c95
Fadi Eliya <fadi@fadi-eliyas-macbook-pro-2.local>
2010-05-15 19:08:29 -0400

    Updating database creation script for example

--- makedb.sql
+++ makedb.sql
@@ -5,21 +5,12 @@ create database twitchoice_production default character set 'utf8';
 create database twitchoice_staging default character set 'utf8';
 
 grant all on twitchoice_development.* to 'tcdev'@'localhost' identified by 'y0ud0ntkn0w';
-
 grant all on twitchoice_test.* to 'tctest'@'localhost' identified by 'y0uw15hy0ukn3w';
-
 grant all on twitchoice_prod.* to 'tcprod'@'localhost' identified by 'y0uw1lln3v3rgu355';
-
 grant all on twitchoice_development.* to 'tcdev'@'%' identified by 'y0ud0ntkn0w';
-
 grant all on twitchoice_test.* to 'tctest'@'%' identified by 'y0uw15hy0ukn3w';
-
 grant all on twitchoice_prod.* to 'tcprod'@'%' identified by 'y0uw1lln3v3rgu355';
-
 grant all on twitchoice_qa.* to 'tcqa'@'localhost' identified by 't3st1ng';
-
 grant all on twitchoice_qa.* to 'tcqa'@'%' identified by 't3st1ng';
-
 grant all on twitchoice_staging.* to 'tcstaging'@'localhost' identified by '5t4g1ng';
-
-grant all on twitchoice_staging.* to 'tcstaging'@'%' identified by '5t4g1ng';
+grant all on twitchoice_staging.* to 'tcstaging'@'%' identified by '5t4g1ng'; #do nothing
