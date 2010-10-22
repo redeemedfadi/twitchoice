ae8542c04d5476e813bb071872109e64fbabd54e
Fadi Eliya <fadi@fadi-eliyas-macbook-pro-2.local>
2010-10-21 22:41:38 -0400

    Question Creation Validation!

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,9 @@ class Question < ActiveRecord::Base
   belongs_to :user
   acts_as_commentable
 
+  validates_presence_of :question
+  validates_length_of :answers, :minimum => 2, :message => "- there must be at least two"
+
   def user_has_voted?(user_id)
     votes.detect { |vote| vote.user_id == user_id } != nil
   end
